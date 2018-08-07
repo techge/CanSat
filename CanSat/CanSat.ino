9aa4f5a6c6464037fe17cbfd3c80abf51d518e2f
techge <techge+github@posteo.net>
2018-08-07 11:22:38 +0200

    Fix problem with global constants for MPU initialization

--- CanSat/CanSat.ino
+++ CanSat/CanSat.ino
@@ -2,30 +2,30 @@
 #include <SPI.h>
 #include "getSensorData.h"
 
-//Adafruit_BMP280 bmp; // I2C
-//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//const byte PWR_MGT_1 = 0x6B;
+//const byte ACCEL_CONFIG = 0x1C;
+
 Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 void setup() {
-
-  Serial.begin(9600);
   
   // ensure BMP is connected
-  if (!bmp.begin()) {  
+  /*if (!bmp.begin()) {  
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
-    //while (1);
-  }
+    //while (1);u
+  }*/
 
   // initialize MPU
   Wire.begin();
   Wire.beginTransmission(MPU_6050_addr);
-  Wire.write(PWR_MGT_1);
+  Wire.write(0x6B);
   Wire.write(0);
   Wire.endTransmission();
   Wire.beginTransmission(MPU_6050_addr);
-  Wire.write(ACCEL_CONFIG);
+  Wire.write(0x1C);
   Wire.write(0x10);
   Wire.endTransmission();
+
   Serial.begin(9600);
   
 }
@@ -33,6 +33,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  Serial.print(millis()); Serial.print(";");
   Serial.print("The temperature (BMP280) is "); Serial.println(getTempBMP(bmp));
   Serial.print("The pressure (BMP280) is "); Serial.println(getPressure(bmp));
   Serial.print("The altitude (BMP280) is "); Serial.println(getAltitude(bmp));
